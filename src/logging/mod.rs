afe000c8909288ad73f5eb5057ff277e7def20fc
astherath <farceriv@gmail.com>
2023-04-23 20:46:57 -0400

    added logging

--- src/logging/mod.rs
+++ src/logging/mod.rs
@@ -1,1 +1,5 @@
+use log4rs;
 
+pub fn init_logger() {
+    log4rs::init_file("logging_config.yaml", Default::default()).unwrap();
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,18 @@
 mod logging;
 <<<<<<< HEAD
+<<<<<<< HEAD
 mod parser;
 mod command;
 =======
 >>>>>>> added logging and removed metrics
+=======
+use log::{info, warn, error};
+
+>>>>>>> added logging
 fn main() {
+    logging::init_logger();
     println!("Hello, world!");
+    info!("test1");
+    warn!("test2");
+    error!("test3");
 }
