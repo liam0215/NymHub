673250497678fde02b1c6e060421ce5a2d0bf37d
astherath <farceriv@gmail.com>
2023-04-26 15:28:36 -0400

    added tests

--- src/sensor/mod.rs
+++ src/sensor/mod.rs
@@ -51,3 +51,42 @@ impl Sensor {
         Ok(())
     }
 }
+
+#[cfg(test)]
+mod test {
+    use crate::common::testing;
+    use log::Level;
+    extern crate testing_logger;
+
+    use super::*;
+
+    async fn get_sensor() -> Sensor {
+        let address = get_test_address();
+        Sensor::init_from_str(&address).await
+    }
+
+    fn validate_logs(expected_logs: &[(&str, Level)]) {
+        let target = "iot_hub::sensor".to_string();
+        testing::validate_logs(target, expected_logs);
+    }
+
+    fn get_test_address() -> String {
+        "5Gu1gnu9uWLfKyHmSQFZU1vgkpeTxWk6VtLKH4eW9h3q.6o48hSHUEsY7qeKwQmD3ZQQ84esUD27QRQwCkYMes1w1@3ojQD6V7skM1bSXJX7fVQvscjmcgptzdixQEaAha2ixh".to_string()
+    }
+
+    #[tokio::test]
+    async fn test_init_sensor() {
+        testing::before_each();
+
+        let sensor = get_sensor().await;
+
+        assert!(sensor.client.identity().to_base58_string().len() != 0);
+        let ex_log_msg = format!(
+            "creating sensor and init mixnet client, pointing at: {}",
+            &get_test_address()
+        );
+        let expected_logs = [(ex_log_msg.as_str(), Level::Info)];
+
+        validate_logs(&expected_logs);
+    }
+}
