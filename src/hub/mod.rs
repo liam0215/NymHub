880e322823480752edbfacaa82e8e71087cecceb
astherath <farceriv@gmail.com>
2023-04-25 20:43:50 -0400

    added base test

--- src/hub/mod.rs
+++ src/hub/mod.rs
@@ -11,6 +11,7 @@ impl Hub {
     pub async fn init() -> Self {
         info!("starting up nym client...");
         let client = mixnet::MixnetClient::connect_new().await.unwrap();
+        info!("established connection with client with hub");
         Self { client }
     }
 
@@ -30,11 +31,37 @@ impl Hub {
 
 #[cfg(test)]
 mod test {
+    use log::Level;
+    extern crate testing_logger;
+
     use super::*;
 
-    #[test]
-    fn test_parse_says() {
-        const MSG: &str = "user1 says ( or ( < temp 70 ) ac_on )\n";
-        let _pairs = NymHubDSL::parse(Rule::main, MSG).unwrap_or_else(|e| panic!("{}", e));
+    fn before_each() {
+        testing_logger::setup();
+    }
+
+    async fn get_hub() -> Hub {
+        Hub::init().await
+    }
+
+    #[tokio::test]
+    async fn test_init_hub() {
+        before_each();
+        let hub = get_hub().await;
+        dbg!(hub.client.identity().to_base58_string());
+        assert!(hub.client.identity().to_base58_string().len() != 0);
+
+        testing_logger::validate(|all_logs| {
+            let target = "iot_hub::hub".to_string();
+            let captured_logs: Vec<&testing_logger::CapturedLog> =
+                all_logs.iter().filter(|x| x.target == target).collect();
+            assert_eq!(captured_logs.len(), 2);
+            assert_eq!(captured_logs[0].body, "starting up nym client...");
+            assert_eq!(
+                captured_logs[1].body,
+                "established connection with client with hub"
+            );
+            assert!(captured_logs.iter().all(|x| x.level == Level::Info));
+        });
     }
 }
